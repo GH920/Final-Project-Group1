d43c99e44538b63e4bda8501987194a8cbfeec2c
Gaofeng HUANG <gaofeng@koukouhous-MacBook-Air.local>
2019-12-03 19:04:15 -0500

    Uploaded folders

---
+++ Code/plot_debugger.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+a = np.linspace(0, 10, 20)
+b = a**2
+plt.plot(a,b)
+plt.show()
--- plot_debugger.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-a = np.linspace(0, 10, 20)
-b = a**2
-plt.plot(a,b)
-plt.show()
